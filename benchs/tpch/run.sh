62c89e754833f63c6ebaa776732cc6ed9ccd3335
Oleg Piskunov <o.piskunov@tarantool.org>
2020-05-29 05:48:14 +0300

    fixing tpch run script

--- benchs/tpch/run.sh
+++ benchs/tpch/run.sh
@@ -3,16 +3,25 @@
 ### set -o pipefail
 
 TAR_VER=$(tarantool -v | grep -e "Tarantool" |  grep -oP '\s\K\S*')
-### numaopts="numactl --membind=1 --cpunodebind=1 --physcpubind=6,7,8,9,10,11"
+numaopts="numactl --membind=1 --cpunodebind=1 --physcpubind=6,7,8,9,10,11"
 base_dir=$(pwd)
+
+cd /opt/tpch
+make TPC-H
 
 killall tarantool tpcc_load 2>/dev/null || true
 sync && echo "sync passed" || echo "sync failed with error" $?
 echo 3 > /proc/sys/vm/drop_caches
 
-cd /opt/tpch
+$numaopts /opt/tpch/bench_queries.sh 2>&1 | tee bench-sqlite.log
+make 00000000000000000000.snap
 
-./make all
+killall tarantool tpcc_load 2>/dev/null || true
+sync && echo "sync passed" || echo "sync failed with error" $?
+echo 3 > /proc/sys/vm/drop_caches
+
+$numaopts tarantool execute_query.lua -n 3 2>&1 | tee bench-tnt.log
+make report
 
 echo ${TAR_VER} | tee tpc.h_t_version.txt
 cp -f tpc.h_t_version.txt $base_dir
