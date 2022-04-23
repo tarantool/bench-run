55b945bfed6a9e1a4de6a63e7116a2845b2f2931
Yaroslav Lobankov <y.lobankov@tarantool.org>
2022-04-23 16:51:56 +0300

    myteam: extend args validation in script
    Add the following validations:
    - the '--message' argument must not be empty
    - the '--file' argument must not be empty
    - the '--message' or '--file' argument must be provided
    - impossible to use '--message' and '--file' arguments at the same time

--- reporting/myteam.py
+++ reporting/myteam.py
@@ -14,8 +14,8 @@ def parse_args():
     parser.add_argument('-u', '--url', default=env_opt('MYTEAM_URL'), help='MyTeam API connection URL')
     parser.add_argument('-t', '--token', default=env_opt('MYTEAM_TOKEN'), help='MyTeam token for API requests')
     parser.add_argument('-c', '--chat-id', default=env_opt('MYTEAM_CHAT_ID'), help='MyTeam chat ID')
-    parser.add_argument('-m', '--message', default='Hi there!', help='Message to send')
-    parser.add_argument('-f', '--file', help='Path to the file with message to send (takes precedence over --message)')
+    parser.add_argument('-m', '--message', help='Message to send')
+    parser.add_argument('-f', '--file', help='Path to the file with message to send')
     parser.add_argument('-F', '--format', choices=['Text', 'MarkdownV2'], default='Text', help='Message format to use')
 
     return parser.parse_args()
@@ -33,6 +33,16 @@ def validate_args(args):
                 f"'MYTEAM_{arg_name.upper()}' env variable must be defined"
             )
 
+    for arg_name in ['message', 'file']:
+        if getattr(args, arg_name) == '':
+            raise ValueError(f"Value for argument '--{arg_name}' must not be empty")
+
+    if not any([args.message, args.file]):
+        raise ValueError("Argument '--message' or '--file' must be provided")
+
+    if all([args.message, args.file]):
+        raise ValueError("Cannot use '--message' and '--file' arguments at the same time")
+
 
 def main(args):
     validate_args(args)
@@ -45,6 +55,8 @@ def main(args):
             # All special characters that do not indicate the beginning or end of a text style must be escaped with
             # a backslash.
             message = f.read().strip().replace('_', '\\_')
+            if not message:
+                raise ValueError('Message file is empty')
 
     if args.format == 'Text':
         # There is no 'Text' parse mode, defined for convenience.
